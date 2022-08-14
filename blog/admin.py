9f0b3fb3d980c67e3a5f1c6bdbb9e374257259ea
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-08-14 19:13:15 +0300

    added image preview to Post admin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
 from blog.models import Post, Tag, Comment
+from django.utils.safestring import mark_safe
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    fields = (
+        'title',
+        'slug',
+        'author',
+        'image',
+        'preview',
+        'text',
+        'tags',
+        'published_at',
+        'likes'
+    )
     list_display = ('title', 'author',)
     raw_id_fields = ('likes',)
     search_fields = ('title', 'author__username', 'published_at',)
+    readonly_fields = ["preview"]
+
+    def preview(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="max-height: 200px;">')
 
 
 @admin.register(Comment)
