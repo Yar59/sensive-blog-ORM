e5e30f86bf5f3d3d3fb42b3333e12e9ade623e13
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-08-14 17:42:31 +0300

    len() replaced with count()

--- blog/views.py
+++ blog/views.py
@@ -65,7 +65,7 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
+    likes = post.likes.all().count()
 
     related_tags = post.tags.annotate(posts_amount=Count('posts'))
 
@@ -74,7 +74,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': likes,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
