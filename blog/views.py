5d3e4752b30241cfa963c2b7e16469a185e8feaa
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-08-14 17:07:40 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(posts_amount=Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -20,7 +20,7 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': tag.posts.count(),
+        'posts_with_tag': tag.posts_amount,
     }
 
 
@@ -35,7 +35,7 @@ def index(request):
 
     most_fresh_posts = list(fresh_posts)[-5:]
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = Tag.objects.popular().annotate(posts_amount=Count('posts'))[:5]
 
     context = {
         'most_popular_posts': [
@@ -60,7 +60,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.annotate(posts_amount=Count('posts'))
 
     serialized_post = {
         'title': post.title,
