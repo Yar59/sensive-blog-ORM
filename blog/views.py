7b74d35b97a4da332e6594f2a3d1ab4b6834a2e6
Yar59 <75260156+Yar59@users.noreply.github.com>
2022-08-14 12:59:43 +0300

    replace sorted func with order_by

--- blog/views.py
+++ blog/views.py
@@ -28,8 +28,7 @@ def serialize_tag(tag):
 
 
 def index(request):
-    all_posts = Post.objects.annotate(likes_amount=Count('likes'))
-    most_popular_posts = sorted(all_posts, key=get_likes_count)[-5:]
+    most_popular_posts = Post.objects.annotate(likes_amount=Count('likes')).order_by('-likes_amount')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -117,7 +116,3 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
-
-
-def get_likes_count(post):
-    return post.likes_amount
